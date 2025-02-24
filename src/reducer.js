cabb323a46d904f952f9b024a731af777123bd12
Melksedeque <freelancer@melksedeque.com.br>
2025-02-24 14:03:00 -0300

    Criação de case de Editar frase com validação de caracteres e verificação de duplicidade

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
 export const ADICIONAR_FRASE = "ADICIONAR_FRASE";
+export const EDITAR_FRASE = "EDITAR_FRASE";
 export const REMOVER_FRASE = "REMOVER_FRASE";
 
 const reducer = (estado, acao) => {
@@ -14,6 +15,18 @@ const reducer = (estado, acao) => {
         alert("Frase salva com sucesso!");
       }
       return [...estado, acao.frase];
+
+    case EDITAR_FRASE:
+      if (acao.fraseNova.length < 20) {
+        alert("A frase deve ter mais de 20 caracteres");
+        return estado;
+      } else if (estado.includes(acao.fraseNova)) {
+        alert("Não são permitidas frases duplicadas");
+        return estado;
+      }
+      return estado.map((frase) =>
+        frase === acao.fraseAntiga ? acao.fraseNova : frase
+      );
 
     case REMOVER_FRASE:
       return estado.filter((frase) => frase !== acao.frase);
