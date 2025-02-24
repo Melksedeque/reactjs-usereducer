7db73f8c864b1290577aa660b44bb0a0b969abbb
Melksedeque <freelancer@melksedeque.com.br>
2025-02-24 13:35:44 -0300

    Criação de modelo inicial apenas com useState()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // Lista de frases (estado)
+  // O usuário pode adicionar novas frases desde que:
+  // A frase possua mais de 20 caracteres
+  // A frase seja única (não pode haver duplicidade)
+  // Usuário pode remover frases
+  // Usuário pode editar frases
+
+  const [frase, setFrase] = useState("");
+  const [listaDeFrases, setListaDeFrases] = useState([]);
+
+  function salvarFrase(e) {
+    e.preventDefault();
+    if (frase.length < 20) {
+      alert("A frase deve ter mais de 20 caracteres");
+      return;
+    } else if (listaDeFrases.includes(frase)) {
+      alert("Não são permitidas frases duplicadas");
+      return;
+    } else {
+      setListaDeFrases([...listaDeFrases, frase]);
+      alert("Frase salva com sucesso!");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={salvarFrase}>
+        <textarea
+          value={frase}
+          onChange={(evento) => setFrase(evento.target.value)}
+          placeholder="Digite uma frase"
+          required
+        />
+        <br />
+        <button type="submit">Salvar frase</button>
+      </form>
+      {listaDeFrases.map((frase, index) => (
+        <div key={index}>
+          <p>{frase}</p>
+          <button
+            onClick={() => {
+              setListaDeFrases(listaDeFrases.filter((f) => f !== frase));
+            }}
+          >
+            Remover
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
